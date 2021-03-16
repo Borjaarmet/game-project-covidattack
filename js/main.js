d81f42b28268e381d9fe517a27c8c711ff7927cb
Borja Armet <borjaarmet@gmail.com>
2021-03-16 19:45:06 +0100

    add button instrutions

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
 document.addEventListener('DOMcontentLoaded', () => {
 
 
-    function printGameOver() {
+    /*function printGameOver() {
         let gameOver = document.getElementById('gameover');
         let canvas = document.querySelector('#mycanvas');
         canvas.style = 'display: none';
         gameOver.style = 'display: block';
-    }
+    }*/
 
-    function printWinScreen() {
+    /*function printWinScreen() {
         let winScreen = document.getElementById('win-screen');
         let canvas = document.querySelector('#mycanvas');
         canvas.style = 'display: none';
         winScreen.style = 'display: block';
-    }
+    }*/
 
     function printGameScreen() {
         const playButton = document.querySelector('#play-button');
         playButton.addEventListener('click', () => {
-            let firstScreen = document.getElementById('first-screen').innerHTML;
-            firstScreen.style = 'display: none';
+            let firstScreen = document.getElementById('first-screen');
+            firstScreen.style.display = 'none';
 
-            let game = document.getElementById('game').innerHTML;
-            game.style = 'display: block';
+            let game = document.getElementById('game');
+            game.style.display = 'block';
 
 
             let canvas = game.querySelector('#mycanvas');
             let ctx = canvas.getContext('2d');
 
         });
+
     };
     printGameScreen()
 
