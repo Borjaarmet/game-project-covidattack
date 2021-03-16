19326eb44e5c38bce5aec006788335278b55cdcd
Borja Armet <borjaarmet@gmail.com>
2021-03-16 15:41:16 +0100

    adding a class game

--- js/Game.js
+++ js/Game.js
@@ -1,16 +1,13 @@
-function buildSplashScreen() {}
+class Game {
+    constructor(options, callback) {
+        this.ctx = options.ctx;
+        this.player = options.player;
+        this.items = undefined;
+        this.cb = callback;
+    }
 
-function startGameClick() {
-    destroySplashScreen();
-    buildGameScreen();
-}
-
-function destroySplashScreen() {
-
-}
-
-function buildGameScreen() {}
-
-function clean() {}
-
-function update() {}
+    drawItems() {
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect = (20, 20, 30, 30)
+    }
+};
