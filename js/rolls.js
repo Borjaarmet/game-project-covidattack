01514125ab591bc6876a9079bc000e4923e6aa98
Borja Armet <borjaarmet@gmail.com>
2021-03-20 12:46:13 +0100

    add new file

---
+++ js/rolls.js
@@ -0,0 +1,37 @@
+class Rolls {
+    constructor(ctx, score) {
+
+        this.ctx = ctx;
+        this.width = 40;
+        this.height = 40;
+        this.x = this.randomX();
+        this.y = 0;
+        this.speedY = this.randomSpeedY();
+        this.interval = undefined;
+        this.score = 0;
+    }
+
+    draw() {
+        this.ctx.fillStyle = "blue";
+        this.ctx.fillRect(this.x, this.y, 40, 40);
+    }
+
+    move() {
+        this.y = this.y + this.speedY;
+        if (this.y > 500) {
+            this.y = this.y % 500;
+        } else if (this.y < 0) {
+            this.y = (this.y + 500) % 500;
+        }
+    }
+
+    startMovingRolls() {
+        this.interval = setInterval(this.move.bind(this), 100);
+    }
+    randomX() {
+        return Math.random() * 900;
+    }
+    randomSpeedY() {
+        return 8 + Math.random() * (-1);
+    }
+}
